35be896d627c432292cb1296d75bb5e038616759
McGhee-Jake <jmcghee@csumb.edu>
2017-11-29 11:40:25 -0800

    initial commit

---
+++ lab13.py
@@ -0,0 +1,18 @@
+# team MakeSmart
+# lab13
+# 
+
+import os
+
+def getArticle():
+    article = "articlez.txt"
+  
+    # Get the programs working directory - see https://stackoverflow.com/questions/5137497/find-current-directory-and-files-directory
+    directory = os.path.dirname(os.path.abspath(__file__)) 
+    
+    path = directory + "\\" + article
+    file = open(path, "rt")
+    text = file.read()
+    print text
+    
+    
